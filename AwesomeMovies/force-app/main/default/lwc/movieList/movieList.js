e06b8e1a329cc08e8aeecca45b30a3131d32953d
Mahmoud Rhazzoul <mrhazzoul@salesforce.com>
2021-11-05 14:27:42 +0100

    Created the movie container component alongside the filterMovie, the movieHeader, the movieList, and the movieListCard

---
+++ AwesomeMovies/force-app/main/default/lwc/movieList/movieList.js
@@ -0,0 +1,9 @@
+/**
+ * Created by mrhazzoul on 5/11/2021.
+ */
+
+import {LightningElement} from 'lwc';
+
+export default class MovieList extends LightningElement {
+
+}
---
+++ AwesomeMovies/force-app/main/default/lwc/moviePreview/moviePreview.js
@@ -0,0 +1,24 @@
+/**
+ * Created by mrhazzoul on 4/11/2021.
+ */
+
+import {LightningElement} from 'lwc';
+
+export default class MoviePreview extends LightningElement {
+    movieSelected = {
+        image: 'https://i.pinimg.com/originals/fc/95/ca/fc95ca81ca34fba083e38fa6406c87be.jpg',
+        title: 'Titanic',
+        rating: 4,
+        type: '+16',
+        description : 'lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum '
+    }
+
+    handleUpdateClick(){
+
+    }
+
+    handleDeleteClick(){
+
+    }
+
+}
